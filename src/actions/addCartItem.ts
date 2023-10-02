bc7bbd11080a7a47c4764ef235e8c93199040058
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-10-02 22:54:35 +0200

    remove space in addCartItem

--- src/actions/addCartItem.ts
+++ src/actions/addCartItem.ts
@@ -3,14 +3,8 @@ import { getCartItems, setCartItems } from '@/utils/cartItems'
 
 export async function addCartItem(cartItem: CartItem) {
   const cartItems: CartItem[] = await getCartItems()
-
   const itemInTheCart = cartItems.find(({ id }) => cartItem.id === id)
-
-  if (itemInTheCart) {
-    return alert('Item is already in the cart')
-  }
-
+  if (itemInTheCart) return alert('Item is already in the cart')
   cartItems.push(cartItem)
-
   await setCartItems(cartItems)
 }
