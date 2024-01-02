a0a8f860ab4f4031af616cd04699bb09da3ea8ec
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2024-01-02 17:49:37 +0100

    remove CartItem type

--- src/actions/addCartItem.ts
+++ src/actions/addCartItem.ts
@@ -2,7 +2,7 @@ import { CartItem } from '@/types'
 import { getCartItems, setCartItems } from '@/utils/cartItems'
 
 export async function addCartItem(cartItem: CartItem) {
-  const cartItems: CartItem[] = await getCartItems()
+  const cartItems = await getCartItems()
   const itemInTheCart = cartItems.find(({ id }) => cartItem.id === id)
   if (itemInTheCart) return alert('Item is already in the cart')
   cartItems.push(cartItem)
